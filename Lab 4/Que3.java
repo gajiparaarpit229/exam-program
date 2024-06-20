9275ed6234c57e3b0f39600dea5b7330dc6b2ea8
gajiparaarpit229 <146694942+gajiparaarpit229@users.noreply.github.com>
2024-06-20 10:08:06 +0530

    Add files via upload

---
+++ Lab 4/Que3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class Que3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int array[]={1,2,3,4,5,8,10};
+        System.out.println("Enter Element to be inserted:");
+        int element = sc.nextInt();
+        int newArray[] = new int[8];
+        // for (int i=0;i<8;i++) {
+        //     if(i<array.length && array[i]>element && array[i-1]<element){
+        //         newArray[i]=element;
+        //     }
+        //     else if(i<array.length && array[i]>element && array[i-1]>element){
+        //         newArray[i]=array[i-1];
+        //     }
+        //     else{
+        //         newArray[i]=array[i];
+        //     }
+        // }
+        // for(int i=0;i<8;i++){
+        //     System.out.println(newArray[i]);
+        // }
+        int index = 0;
+        while(index<array.length && array[index]<element){
+            newArray[index]=array[index];
+            index++;
+        }
+        newArray[index]=element;
+        while(index<array.length){
+            newArray[index+1]=array[index];
+            index++;
+        }
+        for(int i=0;i<8;i++){
+            System.out.println(newArray[i]);
+        }
+    }
+}
